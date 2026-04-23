447193a08072934afec7abe47d11e622ef0bc7f6
alecogni <alessandrocognigni2004@gmail.com>
2026-04-23 17:35:14 +0200

    setup iniziale progetto e gradle

---
+++ ElementalArena/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0" // Plugin per JavaFX
+}
+
+group = "it.unicam.cs.mpgc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // JavaFX
+    implementation("org.openjfx:javafx-controls:21")
+    implementation("org.openjfx:javafx-fxml:21")
+
+    // Per salvare i file (Persistenza JSON)
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    // Sostituisci con il tuo package e la tua classe Main vera
+    mainClass.set("it.unicam.cs.mpgc.rpg126161.Main")
+}
+
+javafx {
+    version = "21"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
